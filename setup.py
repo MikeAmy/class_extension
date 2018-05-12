3b9877089ce2bebebb558663a246535090803e94
Mike Amy <mikeamycoder@gmail.com>
2018-05-12 18:00:55 +0700

    Updated long_description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='class_extension',
     version='1.0.2',
     description='Allows classes to be extended',
+    long_description=open('README.md').read(),
     author='Michael Amy',
     author_email='mikeamycoder@gmail.com',
     url='https://github.com/MikeAmy/class_extension',
